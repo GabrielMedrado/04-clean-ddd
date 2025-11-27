7c135d6e49345de2d9c6012d7c469d138119d74a
Gabriel Medrado <medradoogabriel@gmail.com>
2025-11-27 11:41:32 -0300

    feat(): fundamentos do ddd

---
+++ src/domain/entities/instructor.ts
@@ -0,0 +1,9 @@
+export class Instructor {
+    public id: string;
+    public name: string;
+
+    constructor(name: string, id?: string) {
+        this.name = name;
+        this.id = id ?? crypto.randomUUID();
+    }
+}
---
+++ src/domain/entities/question.ts
@@ -0,0 +1,25 @@
+import { Slug } from "./value-objects/slug";
+
+interface QuestionProps {
+    title: string;
+    content: string;
+    slug: Slug;
+    authorId: string;
+}
+
+export class Question {
+    public title: string;
+    public content: string;
+    public id: string;
+    public slug: Slug;
+    public authorId: string;
+
+    constructor(props: QuestionProps, id?: string) {
+        this.title = props.title;
+        this.content = props.content;
+        this.authorId = props.authorId;
+        this.slug = props.slug;
+        this.id = id ?? crypto.randomUUID();
+
+    }
+}

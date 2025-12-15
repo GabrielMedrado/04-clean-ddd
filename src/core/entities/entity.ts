6ad970fac3d379f97fd8ad8b507a96960095795c
Gabriel Medrado <medradoogabriel@gmail.com>
2025-12-15 11:40:24 -0300

    feat:() id of entities

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,13 +1,15 @@
+import { UniqueEntityId } from "./unique-entity-id";
+
 export class Entity<Props> {
-    private _id: string;
+    private _id: UniqueEntityId;
     protected props: Props;
 
-    get id(): string {
+    get id() {
         return this._id;
     }
 
     constructor(props: Props, id?: string) {
         this.props = props;
-        this._id = id ?? crypto.randomUUID();
+        this._id = new UniqueEntityId(id);
     }
 }

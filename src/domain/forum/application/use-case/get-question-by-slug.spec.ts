be0c95f691c5683e54afbdb7016408500c44e7e2
Gabriel Medrado <medradoogabriel@gmail.com>
2026-01-26 10:33:33 -0300

    test() fix teste unitário de perguntas

--- src/domain/forum/application/use-case/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-case/get-question-by-slug.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memorry-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { makeQuestion } from 'test/factories/make-question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
@@ -11,16 +12,19 @@ describe('Get Question By Slug', () => {
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
   })
   it('should be able to get a question by slug', async () => {
-    const newQuestion = makeQuestion()
+    const newQuestion = makeQuestion({
+      slug: Slug.create('example-question'),
+    })
+
+    console.log(newQuestion)
 
     inMemoryQuestionsRepository.create(newQuestion)
 
     const { question } = await sut.execute({
-      slug: 'question-title',
+      slug: 'example-question',
     })
 
     expect(question.id).toBeTruthy()
-    expect(inMemoryQuestionsRepository.items[0]?.id).toEqual(question.id)
-    expect(question.slug.value).toEqual(newQuestion.title)
+    expect(question.title).toEqual(newQuestion.title)
   })
 })
